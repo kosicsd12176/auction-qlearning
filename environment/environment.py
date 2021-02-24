64af42072b2b6c5a5d98cc771b12f51fad8119f4
Thodoris Goulas <t.goulas@paymentcomponents.com>
2021-02-24 02:42:48 +0200

    wip

--- environment/environment.py
+++ environment/environment.py
@@ -1,4 +1,3 @@
-import itertools
 import random
 
 import numpy as np
@@ -15,11 +14,12 @@ class Environment(Env):
         self.action_space = Discrete(2)
         self.observation_space = Box(low=np.array([min(C_REWARD, D_REWARD)]), high=np.array([max(A_REWARD, B_REWARD)]),
                                      dtype=int)
-        self.state = random.choice(list(itertools.product([0, 1], [0, 1])))
+        self.state = random.choice([0, 1, 2, 3])
         self.episode_step = MAX_STEPS_PER_EPISODE
 
     def step(self, action: Action):
-        self.state = (action.agent1_action, action.agent2_action)
+        self.state = 2 * action.agent1_action + action.agent2_action if action.agent == 0 \
+            else 2 * action.agent2_action + action.agent1_action
         self.episode_step += -1
 
         if action.agent == 0:
@@ -38,7 +38,7 @@ class Environment(Env):
         return self.state, reward, done, info
 
     def reset(self):
-        self.state = random.choice(list(itertools.product([0, 1], [0, 1])))
+        self.state = random.choice([0, 1, 2, 3])
         self.episode_step = MAX_STEPS_PER_EPISODE
         return self.state
 
--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,4 @@
-import random
-
-NUM_EPISODES = 5
+NUM_EPISODES = 1500
 MAX_STEPS_PER_EPISODE = 100
 LEARNING_RATE = 0.1  # a parameter
 DISCOUNT_RATE = 0.99  # gamma parameter
@@ -11,8 +9,8 @@ MIN_EXPLORATION_RATE = 0
 EXPLORATION_DECAY_RATE = 0.01
 
 
-A_REWARD = random.randint(0, 100)
-B_REWARD = random.randint(0, 100)
+A_REWARD = 1  # random.randint(0, 100)
+B_REWARD = 1  # random.randint(0, 100)
 
-C_REWARD = random.randint(-100, min(A_REWARD, B_REWARD))
-D_REWARD = random.randint(-100, min(A_REWARD, B_REWARD))
+C_REWARD = -1  # random.randint(-100, min(A_REWARD, B_REWARD))
+D_REWARD = -1  # random.randint(-100, min(A_REWARD, B_REWARD))
